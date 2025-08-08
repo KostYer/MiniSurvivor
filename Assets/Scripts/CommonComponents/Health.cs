32ec0642de584e39fd065e2d6149872ec34d3230
KostYer <KostYer@yahoo.com>
2025-08-08 16:19:02 +0300

    [ADD] health settings

--- Assets/Scripts/CommonComponents/Health.cs
+++ Assets/Scripts/CommonComponents/Health.cs
@@ -1,9 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using Settings;
+using UnityEngine;
 
 namespace PlayerRelated
 {
     public class Health: MonoBehaviour
     {
+        public event Action OnHealthDepleted = default;
         
+        private float _maxHealth;
+        private float _health;
+
+        private float HealthValue => _health;
+
+        public void Initialize(HealthSettings settings)
+        {
+            _maxHealth = settings.MaxHealth;
+            _health = settings.MaxHealth;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            _health -= damage;
+            if (_health <= 0f)
+            {
+                OnHealthDepleted?.Invoke();
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Settings/HealthSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Settings
+{
+    [CreateAssetMenu(fileName = "HealthSettingsSO", menuName = "Settings/HealthSettings")]
+    public class HealthSettings: ScriptableObject
+    {
+        [SerializeField] private float _maxHealth;
+
+        public float MaxHealth => _maxHealth;
+    }
+}
