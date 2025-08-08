1696d694adc89f8892f8c8ec4d55f5ed9d012e1b
KostYer <KostYer@yahoo.com>
2025-08-08 12:53:40 +0300

    [ADD] bullet logic

---
+++ Assets/Scripts/CommonComponents/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayerRelated
+{
+    [RequireComponent(typeof(Rigidbody))]   
+    public class Bullet: MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rb;
+        private float _damage;
+
+        private void OnValidate()
+        {
+          _rb = GetComponent<Rigidbody>();
+        }
+
+        public void LaunchBullet(float dmg, float speed, Vector3 direction)
+        {
+           
+            if (_rb == null) return;
+            
+            _damage = dmg;
+            transform.forward = direction;
+            _rb.AddForce(direction * speed, ForceMode.Impulse);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log($"[Bullet] OnTriggerEnter");
+        }
+    }
+}
