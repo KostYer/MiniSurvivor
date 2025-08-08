1e207e52fa1bca781a168b3aa2a0332835210f55
KostYer <KostYer@yahoo.com>
2025-08-08 12:55:31 +0300

    [ADD] shoot settins

---
+++ Assets/Scripts/Settings/ShootSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Settings
+{
+    [CreateAssetMenu(fileName = "ShootSettingsSO", menuName = "Settings/ShootSettings")]
+    public class ShootSettings : ScriptableObject
+    {
+        [SerializeField] private float _shootRange;
+        [SerializeField] private float _shootRate;
+        [SerializeField] private float _damage;
+
+        public float ShootRange => _shootRange;
+        public float ShootRate => _shootRate;
+        public float Damage => _damage;
+    }
+}
