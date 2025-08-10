6464e57522916327739c67b2d0cb4ff8184111d8
KostYer <KostYer@yahoo.com>
2025-08-10 22:30:19 +0300

    [ADD] stats screen

--- Assets/Scripts/UI/MainMenuUIController.cs
+++ Assets/Scripts/UI/MainMenuUIController.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using Enemies;
 using UnityEngine;
+using Waves;
 
 namespace UI
 {
     public class MainMenuUIController: MonoBehaviour
     {
         public event Action OnStartPressed;
+        public event Action OnWaveEndConfirmed;
 
         [SerializeField] private CanvasGroup _mainMenuCanvas;
         [SerializeField] private CanvasGroup _joystickCanvas;
+        [SerializeField] private CanvasGroup _wavesCanves;
+        [SerializeField] private CanvasGroup _timerCanvas;
+        [SerializeField] private StatsScreen _statsScreen;
+        [SerializeField] private TimerUI _timer;
+
+        public TimerUI Timer => _timer;
 
         private void Awake()
         {
@@ -21,9 +31,28 @@ namespace UI
             OnStartPressed?.Invoke();
         }
 
-
         private void ShowCanvasGroup(CanvasGroup cv, bool show)
         {
+            cv.alpha = show? 1 : 0;
+            cv.interactable = show;
+            cv.blocksRaycasts = show;
+        }
+
+        public void WaveConfirmClick()
+        {
+            OnWaveEndConfirmed?.Invoke();
+            ShowCanvasGroup(_wavesCanves, false);
+            ShowCanvasGroup(_joystickCanvas, true);
+            ShowCanvasGroup(_joystickCanvas, true);
+            ShowCanvasGroup(_timerCanvas, true);
+        }
+
+        public void OnWaveDefeated(Dictionary<EnemyType, WaveStatsUnit> stats)
+        {
+            _statsScreen.Initialize(stats);
+            ShowCanvasGroup(_wavesCanves, true);
+            ShowCanvasGroup(_joystickCanvas, false);
+            ShowCanvasGroup(_timerCanvas, false);
         }
     }
 }
---
+++ Assets/Scripts/UI/StatsScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Enemies;
+using TMPro;
+using UnityEngine;
+using Waves;
+
+namespace UI
+{
+    public class StatsScreen: MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _result;
+        [SerializeField] private TMP_Text _timer;
+        [SerializeField] private TMP_Text _buttonText;
+        [SerializeField] private List<TMP_Text> _enemyHeaders = new();
+        [SerializeField] private List<TMP_Text> _enemyCounters = new();
+
+
+        public void Initialize(Dictionary<EnemyType, WaveStatsUnit> stats)
+        {
+            int i = 0;
+            foreach (var kvp in stats)
+            {
+                _enemyHeaders[i].text = "Enemy " + kvp.Value.EnemyType;
+                _enemyCounters[i].text = kvp.Value.CountKilled.ToString();
+                i++;
+            }
+        }
+    }
+}
